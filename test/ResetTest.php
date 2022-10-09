9d3c1db18e7c35536dba414f68561efb54bc8e15
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-09 23:51:00 +0700

    Reset Handler

---
+++ test/ResetTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Test\PHPLOGGER;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\HostnameProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Test\TestCase;
+
+class ResetTest extends TestCase{
+    public function testReset(){
+        $logger=new Logger(ResetTest::class);
+        $logger->pushHandler(new StreamHandler("php://stderr"));
+        $logger->pushHandler(new StreamHandler(__DIR__."/../application.log"));
+        $logger->pushProcessor(new MemoryUsageProcessor());
+        $logger->pushProcessor(new HostnameProcessor());
+
+        for($i=0;$i<10000;$i++){
+            $logger->info("Perulangan ke $i");
+            if($i % 100 == 0){
+                $logger->reset();
+            }
+        }
+        
+        self::assertNotNull($logger);
+    }
+}
